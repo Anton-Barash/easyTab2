90edc51c2b3ec0b672c7a9218e448948c880f2b9
Anton-Barash <anton-barash@users.noreply.github.com>
2026-08-24 21:20:38 +0800

    chore(auth_provider): add flutter foundation import
    add required foundation import for Dart/Flutter auth provider code

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../l10n/app_localizations.dart';
 import '../services/api_service.dart';
